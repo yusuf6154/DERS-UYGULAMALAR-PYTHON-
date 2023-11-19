bf39b6ddd64b9bbfc549d4351799aaf00180e716
Yusuf Can ASLANTÜRK <150507196+yusufaslanturkkk@users.noreply.github.com>
2023-11-19 13:48:47 +0300

    4

--- DERS İÇİ UYGULAMALAR/dizi.py
+++ DERS İÇİ UYGULAMALAR/dizi.py
@@ -1,8 +1,11 @@
 import random
 sorular = ["doruk","eren","kıvanç","yusuf"]
 cevaplar = ["e","w","r","a"]
-print(random.choise(sorular))
-for x in(sorular):
-    print(x)
-print(len(sorular))
-print(sorular[2])
+soru = random.randint(0,len(sorular)-1)
+print(sorular[soru])
+cevap = input ()
+if cevap == cevaplar
+# for x in(sorular):
+    # print(x)
+# print(len(sorular))
+# print(sorular[2])
