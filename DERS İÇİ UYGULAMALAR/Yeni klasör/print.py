4a28ad5f7eb9743419af09d1407fff05f3d12b9a
Yusuf Can ASLANTÜRK <150507196+yusufaslanturkkk@users.noreply.github.com>
2023-11-26 12:53:54 +0300

    deneme

---
+++ DERS İÇİ UYGULAMALAR/Yeni klasör/print.py
@@ -0,0 +1,6 @@
+ad = input("adınızı yazınız:")
+sehır = Ankara
+# print(f"Merhaba {ad},{sehır} sehrını gezmek ıster mısın?")
+meyveler = ["elma","armut","muz"]
+for meyve in meyveler:
+    print(meyve,end="")
